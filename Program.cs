3cc8b273e998d6a5a2417230996dbfa02fbb7b70
Tim Mulholland <timulhol@microsoft.com>
2020-11-17 14:02:56 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleEndLineProof
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                (int x, int y) = Console.GetCursorPosition();
+                Console.SetCursorPosition(0, 0);
+                Console.Write($"{x},{y}");
+                Console.SetCursorPosition(x, y);
+                var keyInfo = Console.ReadKey(intercept: true);
+                Console.Write(keyInfo.KeyChar);
+            }
+        }
+    }
+}
